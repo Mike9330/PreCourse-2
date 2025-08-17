7bc5fdae2016f8f77f03f952cd549a1244671ced
michaeldonato <mmd1130@gmail.com>
2025-08-17 11:25:17 -0400

    Corrected Binary Search logic, added comments

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,21 +1,27 @@
-// Time Complexity : O(n)
-// Space Complexity :
+// Time Complexity : O(logn)
+// Space Complexity :O(1)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
-    { 
-        while (l < r) {
+    {
+        // While the leftmost point of the array is less than or equal to the rightmost
+        while (l <= r) {
+
+            // Finds middle point of array
             int mid = l + (r - l) / 2;
-            
+
+            //If the midpoint is the target, return it
             if (arr[mid] == x) {
                 return mid;
             } else {
+                // If the target is greater than the midpoint, update the left index to be one ahead of the midpoint
                 if (arr[mid] < x) {
-                    l += 1;
+                    l = mid + 1;
+                    // If the target is less than the midpoint, update the right index to be one less the midpoint
                 } else if (arr[mid] > x) {
-                    r -= 1;
+                    r  = mid - 1;
                 }
             }
         }
